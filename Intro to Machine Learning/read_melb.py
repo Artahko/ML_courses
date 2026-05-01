b55f6aaadf28ac79f6723528f774a4de309fcb34
Artahko <artem4iky8@gmail.com>
2026-05-01 19:31:06 +0300

    completed task Your First Machine Learning Model

--- Intro to Machine Learning/read_melb.py
+++ Intro to Machine Learning/read_melb.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
-melbourne_data = pd.read_csv("melb_data.csv")
+melbourne_file_path = "melb_data.csv"
+melbourne_data = pd.read_csv("")
 
+print(melbourne_data.columns)
 print(melbourne_data)
+
+melbourne_data = melbourne_data.dropna(axis=0)
